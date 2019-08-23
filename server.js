a56fc69bc9e82ea789ce86f95687e74600943667
Virento Voshe <30956131+userinfamous@users.noreply.github.com>
2019-08-23 18:05:49 +0700

    Production

--- server.js
+++ server.js
@@ -1,12 +1,1 @@
-var connect = require('connect'),
-    serveStatic = require('serve-static');
-
-console.log('\n\n--- Node Version: ' + process.version + ' ---');
-
-// Set up Connect routing
-var app = connect()
-
-app.use(serveStatic("../coding-club-website"));
-
-// Start node server listening on specified port -----
-app.listen(80);
+var compression=require('compression');var connect=require('connect');var serveStatic=require('serve-static');console.log('\n\n--- Node Version: '+process.version+' ---');var app=connect();app.use(compression({filter:shouldCompress}));app.use(serveStatic("."));app.listen(80);function shouldCompress(req,res){if(req.headers['x-no-compression']){return false}return compression.filter(req,res)}
