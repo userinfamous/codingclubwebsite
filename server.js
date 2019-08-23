c7c96dce743ad354030271b1c22f0728a682c10c
Virento Voshe <30956131+userinfamous@users.noreply.github.com>
2019-08-23 18:52:01 +0700

    server.js changes

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-var compression=require('compression');var connect=require('connect');var serveStatic=require('serve-static');console.log('\n\n--- Node Version: '+process.version+' ---');var app=connect();app.use(compression({filter:shouldCompress}));app.use(serveStatic("."));app.listen(80);function shouldCompress(req,res){if(req.headers['x-no-compression']){return false}return compression.filter(req,res)}
+var compression = require('compression');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+const PORT = process.env.PORT || 5000
+console.log('\n\n--- Node Version: ' + process.version + ' ---');
+
+// Set up Connect routing
+var app = connect()
+app.use(compression({
+    filter: shouldCompress
+}))
+app.use(serveStatic("."));
+app.listen(PORT);
+
+
+function shouldCompress(req, res) {
+    if (req.headers['x-no-compression']) {
+        // don't compress responses with this request header
+        return false
+    }
+    // fallback to standard filter function
+    return compression.filter(req, res)
+}
